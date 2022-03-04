37af91e54a1e0348751356109ee62104b4c31128
Edouard Levasseur <lewardo23@gmail.com>
2022-03-04 21:52:22 +0000

    config files

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,48 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  publicPath: process.env.PUBLIC_URL || "/",
+  filenameHashing: false,
+  configureWebpack: {
+    output: {
+      filename: "js/[name].js",
+      chunkFilename: "js/[id].js"
+    },
+    resolve: {
+      extensions: ['.js', '.vue', '.json', '.sass'],
+      alias: {
+        '@': path.resolve('src'),
+        '@components': path.resolve('src/components'),
+        '@views': path.resolve('src/views'),
+        '@styles': path.resolve('src/styles'),
+      }
+    },
+    module: {
+      rules: [{
+        test: /\.s[ac]ss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              indentedSyntax: true,
+              sassOptions: {
+                indentedSyntax: true
+              }
+            }
+          }
+        ]
+      }, {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+        include: /src/,
+        options: {
+          appendTsSuffixTo: [/\.vue$/],
+        }
+      }]
+    },
+  }
+});
